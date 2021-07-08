97e338a7b5d4341a2c1e4108de9caa698d5f2c1d
Kalkidan Alemu <kali.taye@gmail.com>
2021-07-07 23:32:37 -0400

    Row Style

--- src/Row.js
+++ src/Row.js
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import axios from './axios'
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(()=> {
-        //if [], run once when the row loads, and don't run again
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
-console.log (movies);
+  useEffect(() => {
+    //if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
 
-    return(
-        <div>
-            <h2>{title}</h2>
-            {/* container -> posters */}
-        </div>
-
-    )
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="row_posters"></div>
+      {/* container -> posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
