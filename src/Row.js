6b148771a3a11043a00e558568010af72ea3cc36
Kalkidan Alemu <kali.taye@gmail.com>
2021-07-08 00:16:18 -0400

    Fetch  movie images for Rows

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-function Row({ title, fetchUrl }) {
+import "./row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -15,9 +19,20 @@ function Row({ title, fetchUrl }) {
   console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-      <div className="row_posters"></div>
+      <div className={`row_posters ${isLargeRow && "row_posterLarge"}`}>
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/* container -> posters */}
     </div>
   );
